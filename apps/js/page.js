781da0cf2fe183227520aa40424c6dee1e50eade
lv1codeman <lv1codeman@gmail.com>
2019-06-22 05:10:47 +0800

    1. 修改toTopArrow位置 2. navbar高度設為5vh, footer高度設為2.5vh, slick高度設為92.5vh 3. events.html 自訂RWD效果 4. page.js 加入getWidth(), 監看畫面寬度 5. 設定Hamburger Toggler的border color(navbar-toggler-color @page.css), 讓漢堡盒有外框比較明顯 6. footer文字水平垂直置中by display:flex

---
+++ apps/js/page.js
@@ -0,0 +1,21 @@
+﻿function getWidth() {
+    var s = "";
+    s += "網頁可見區域寬：" + (document.body.clientWidth + 18);
+    s += "<br/>網頁可見區域高：" + document.body.clientHeight;
+    return s;
+}
+
+
+/* 
+//引用此JS
+<script type="text/javascript" src="js/page.js"></script>
+
+//放在script中
+$(window).resize(function () {
+    $("#winWidth").html("<p>" + getWidth() + "</p>");
+});
+
+//在html中顯示就設block，隱藏就設none
+<div id="winWidth" style="display: block; font-family: Arial;">resize windows to show the width changing value here.</div>
+
+*/
